3d3f3f3bfafda6307403e40fa0705055f7185e72
Shubham Vishwakarma <sv09734@gmail.com>
2024-09-26 20:16:47 +0530

    dashboard page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  return <>Hi</>;
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
 }
